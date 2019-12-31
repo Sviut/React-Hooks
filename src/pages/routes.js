a516a64d4013627af4bcbe9353f0237780380a97
Sviut <44402566+Sviut@users.noreply.github.com>
2019-12-31 15:24:10 +0300

    add login page and auth routing

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -3,11 +3,14 @@ import React from 'react'
 
 import GlobalFeed from './globalFeed'
 import Article from './article'
+import Auth from './auth'
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact/>
+      <Route path="/login" component={Auth} />
+      <Route path="/register" component={Auth} />
       <Route path="/articles/:slug" component={Article} />
     </Switch>
   )
