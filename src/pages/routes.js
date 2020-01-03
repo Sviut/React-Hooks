ed19262cfa53e57f21cccab38c67acc3a2505154
Sviut <Sviut@users.noreply.github.com>
2020-01-03 19:16:45 +0300

    add tag feed page

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -4,11 +4,13 @@ import React from 'react'
 import GlobalFeed from './globalFeed'
 import Article from './article'
 import Auth from './auth'
+import TagFeed from './tagFeed'
 
 export default () => {
   return (
     <Switch>
       <Route path="/" component={GlobalFeed} exact/>
+      <Route path="/tags/:slug" component={TagFeed} />
       <Route path="/login" component={Auth} />
       <Route path="/register" component={Auth} />
       <Route path="/articles/:slug" component={Article} />
